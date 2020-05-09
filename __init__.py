ab705775e384a115f0cbe5098da9dac23f595f7b
hana bouzid <hana.bouzid95@gmail.com>
2020-05-09 20:50:44 +0100

    first commit

--- __init__.py
+++ __init__.py
@@ -64,7 +64,7 @@ class RegSkill(MycroftSkill):
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    '/opt/mycroft/skills/testregskill.hanabouzid/client_secret.json', SCOPES)
+                    '/opt/mycroft/skills/regskill.hanabouzid/client_secret.json', SCOPES)
                 creds = flow.run_local_server(port=0)
                 # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
