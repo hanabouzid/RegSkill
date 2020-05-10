ff4e0b611131dbef448385c640824d68687983db
hana bouzid <hana.bouzid95@gmail.com>
2020-05-10 23:38:18 +0100

    first commit

--- __init__.py
+++ __init__.py
@@ -131,6 +131,7 @@ class RegSkill(MycroftSkill):
         attendees = []
         namerooms = ['Midoune Room','Aiguilles Room','Barrouta Room','Kantaoui Room','Gorges Room','Ichkeul Room','Khemir Room','Tamaghza Room','Friguia Room','Ksour Room','Medeina Room','Thyna Room']
         emailrooms = ["focus-corporation.com_3436373433373035363932@resource.calendar.google.com","focus-corporation.com_3132323634363237333835@resource.calendar.google.com","focus-corporation.com_3335353934333838383834@resource.calendar.google.com","focus-corporation.com_3335343331353831343533@resource.calendar.google.com","focus-corporation.com_3436383331343336343130@resource.calendar.google.com","focus-corporation.com_36323631393136363531@resource.calendar.google.com","focus-corporation.com_3935343631343936373336@resource.calendar.google.com","focus-corporation.com_3739333735323735393039@resource.calendar.google.com","focus-corporation.com_3132343934363632383933@resource.calendar.google.com","focus-corporation.com_@resource.calendar.google.com","focus-corporation.com_@resource.calendar.google.com","focus-corporation.com_@resource.calendar.google.com"]
+        indiceroom =None
         for j, e in enumerate(namerooms):
             if e == location:
                 indiceroom=j
@@ -153,6 +154,7 @@ class RegSkill(MycroftSkill):
             nameliste.append(names[0].get('displayName'))
         #recherche des mails des invités
         for i in listp:
+            indiceperson=None
             for j, e in enumerate(nameliste):
                 if e == i:
                     indiceperson=j
